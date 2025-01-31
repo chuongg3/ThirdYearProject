b3f300015201e5f3652868cf5db998f72b38e6c6
Tuan Chuong Goh <chuong272003@gmail.com>
2025-01-31 13:54:13 +0000

    Added scripts to collect data
    These scripts checks looks for bitcode files in the benchmark directory
    and runs each benchmark

---
+++ scripts/FindBenchmark.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script is used to find all benchmark bitcode files in the benchmark's base directory
+# and calls a specified script on each bitcode file and saves a log file in the same location
+
+# Technique Options:
+# - f3m
+# - f3m-adapt
+# - hyfm
+# - baseline
+
+set -x
+
+search_dir=$1
+script_loc=$2
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <benchmark_base_directory> <script_location> <technique>"
+    exit 1
+fi
+
+# Validate technique argument
+echo "Technique Argument: $3"
+technique=$3
+if [[ "$technique" != "f3m" && "$technique" != "f3m-adapt" && "$technique" != "hyfm" && "$technique" != "baseline" ]]; then
+    echo "Invalid technique: $technique"
+    echo "Valid options are: f3m, f3m-adapt, hyfm, baseline"
+    exit 1
+fi
+
+find "$search_dir" -type f -name "_main_._all_._files_._linked_.bc" -exec sh -c '. "$0" "$(dirname "$1")" "$2" | tee ./script_log.txt' "$script_loc" {} "$technique" \;
---
+++ scripts/RunBenchmark.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Runs a benchmark given the directory and technique
+# Usage: RunBenchmark.sh <benchmark_directory> <technique>
+
+# First Parameter: Directory of benchmark
+# Sets environment variable of benchmark name and runs the benchmark
+
+set -x
+
+# Check if the number of arguments is correct
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <benchmark_directory> <technique>"
+    exit 1
+fi
+
+# Set the benchmark directory
+BENCHMARK_LOC=$1
+BENCHMARK_DIR=$(dirname "$BENCHMARK_LOC")
+BENCHMARK=$(basename "$BENCHMARK_DIR")
+
+echo "Benchmark Directory: $BENCHMARK_DIR"
+
+# Set the current directory
+CURRENT_DIR=$(pwd)
+cd "$BENCHMARK_DIR" || { echo "Failed to change directory to $BENCHMARK_DIR"; exit 1; }
+
+MERGE_TECHNIQUE=$2
+
+# Run the benchmark
+echo "Running benchmark: $BENCHMARK"
+/usr/bin/make FROM_SCRATCH=false REPORT=true LLVM_DIR=/home/chuongg3/Projects/TYP/build/bin TECHNIQUE=$2 MERGE_TECHNIQUE="$MERGE_TECHNIQUE" BENCH="$BENCHMARK" 2>&1
+
+# echo "Finished running benchmark: $BENCHMARK"
+
+# Unset the benchmark directory
+echo "Unsetting benchmark directory"
+cd "$CURRENT_DIR" || { echo "Failed to change directory to $CURRENT_DIR"; exit 1; }
+unset BENCHMARK
+unset CURRENT_DIR
+unset BENCHMARK_DIR
+unset MERGE_TECHNIQUE
+
+exit 0

38606ae3fec2118159108c0dea8ee7fc0b2f917d
Tuan Chuong Goh <chuong272003@gmail.com>
2025-03-10 17:32:52 +0000

    [Model] DotProd SiameseModel Dense Final Layer
    Modified the DotProd SiameseModel to use a dense layer instead of
    just activation layer

--- Model/models/DotProdSiameseModel.py
+++ Model/models/DotProdSiameseModel.py
@@ -12,27 +12,27 @@ from TrainFunctions import DumpHistory
 from LoadData import CreateTensorflowDataset, CreateNumpyDataset
 
 # Shared encoder with multiple Dense layers
-def encoder(input_shape = (300,), dropout = 0.3):
+def encoder(input_shape = (300,), dropout = 0.3, units = [512, 128]):
     inputs = Input(shape=input_shape)
 
-    x = Dense(512, activation="relu")(inputs)
+    x = Dense(units[0], activation="relu")(inputs)
     x = Dropout(dropout)(x)
 
-    x = Dense(128, activation="relu")(x)
-    x = Dropout(dropout)(x)
+    x = Dense(units[1], activation="relu")(x)
 
-    x = Dense(64, activation="relu")(x)
+    normalizer = BatchNormalization(axis=-1)
+    x = normalizer(x)
 
     return Model(inputs, x)
 
-def get_model(loss="mean_squared_error", optimizer="adam", learning_rate=0.001, metrics = ['mse', 'mae', 'mape'], dropout=0.25, units=[]):
+def get_model(loss="mean_squared_error", optimizer="adam", learning_rate=0.001, metrics = ['mse', 'mae', 'mape'], dropout=0.25, units=[512, 128]):
     print(" ===== Creating Dot Product Siamese Model =====")
 
     # Define input shape
     input_shape = (300,)
 
     # Create the base network
-    shared_encoder = encoder(input_shape=input_shape, dropout=dropout)
+    shared_encoder = encoder(input_shape=input_shape, dropout=dropout, units=units)
 
     # Inputs for two function embeddings
     input1 = Input(shape=input_shape)
@@ -42,16 +42,11 @@ def get_model(loss="mean_squared_error", optimizer="adam", learning_rate=0.001, 
     encoded1 = shared_encoder(input1)
     encoded2 = shared_encoder(input2)
 
-    # Add normalization layer to the encoded output
-    normalizer = BatchNormalization(axis=-1)
-    normalized_encoded1 = normalizer(encoded1)
-    normalized_encoded2 = normalizer(encoded2)
-
     # Compute dot product similarity
-    dot_product = Dot(axes=1)([normalized_encoded1, normalized_encoded2])
+    dot_product = Dot(axes=1, normalize=True)([encoded1, encoded2])
 
     # Normalize with sigmoid to ensure output is in [0,1]
-    similarity_score = Activation("sigmoid")(dot_product)
+    similarity_score = Dense(1, activation='sigmoid')(dot_product)
 
     # Create model
     model = Model(inputs=[input1, input2], outputs=similarity_score)
@@ -85,8 +80,6 @@ def HyperParameterTraining(DATA_PATH, metrics = ['mse', 'mae', 'mape'], n_trials
     histories = []
     bestModelScore = float("inf")
     print(f"Hyperparameter Training with {n_trials} trials")
-    print(f"Zero Weight: {zero_weight}")
-    print(f"Non-Zero Weight: {non_zero_weight}")
 
     # Define the objective function
     def objective(trial):
@@ -97,11 +90,13 @@ def HyperParameterTraining(DATA_PATH, metrics = ['mse', 'mae', 'mape'], n_trials
         # Get a range of values
         epochs = trial.suggest_int('epochs', 2, 10)
         dropout = trial.suggest_float('dropout', 0.1, 0.5)
-        # batch_size = trial.suggest_int('batch_size', 32, 512, step=16)
         batch_size = trial.suggest_categorical('batch_size', [64, 100])
         learning_rate = trial.suggest_float('learning_rate', 1e-5, 1e-2)
-        # optimizer = trial.suggest_categorical('optimizer', ['adam', 'sgd'])
         optimizer = trial.suggest_categorical('optimizer', ['adam'])
+        units = [
+            trial.suggest_categorical('units_1', [512, 1024]),
+            trial.suggest_categorical('units_2', [64, 128])
+        ]
 
         # Early Stopper to prevent overfitting
         early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
@@ -111,14 +106,16 @@ def HyperParameterTraining(DATA_PATH, metrics = ['mse', 'mae', 'mape'], n_trials
         Dropout: {dropout}
         Batch Size: {batch_size}
         Learning Rate: {learning_rate}
-        Optimizer: {optimizer}""")
+        Optimizer: {optimizer}
+        Units: {units}
+        """)
 
         # Load the dataset
         # training_set, validation_set, test_set = CreateTensorflowDataset(DATA_PATH, batch_size=batch_size, overwrite=False, zero_weight=zero_weight, non_zero_weight=non_zero_weight)
         training_set, validation_set, test_set = CreateNumpyDataset(DATA_PATH, batch_size=batch_size, overwrite=False, zero_weight=zero_weight, non_zero_weight=non_zero_weight)
 
         # Create the model
-        model = get_model(loss="mean_squared_error", optimizer=optimizer, learning_rate=learning_rate, metrics=metrics, dropout=dropout)
+        model = get_model(loss="mean_squared_error", optimizer=optimizer, learning_rate=learning_rate, metrics=metrics, dropout=dropout, units=units)
         startTime = time.time()
         history = model.fit(training_set, epochs=epochs, validation_data=validation_set, callbacks=[early_stopping])
         totalTime = time.time() - startTime

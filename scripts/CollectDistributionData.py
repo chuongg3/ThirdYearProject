e6dca956ddc56deae67b2a3d02bdf75f86f216c7
Tuan Chuong Goh <chuong272003@gmail.com>
2025-04-15 16:38:31 -0500

    [Script] Update Collect Log Data Script

--- scripts/CollectDistributionData.py
+++ scripts/CollectDistributionData.py
@@ -1,176 +1,83 @@
 import re
 import os
-import sqlite3
+import pandas
 import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Collect distribution data")
-    parser.add_argument('--log_dir', '-ld', type=str, default="../f3m_exp/logs/", help='Path to the data file')
-    parser.add_argument('--output', '-o', type=str, default="./MatchInfo.db", help='Output directory for logs and models')
+    parser.add_argument('--log_dir', '-ld', type=str, default="../f3m_exp/logs/", help='Path to the log files')
+    parser.add_argument('--output', '-o', type=str, default="./MatchInfo.csv", help='Output directory for logs and models')
     return parser.parse_args()
 
-digits_pattern = re.compile(r"\d+")
+re_filename = re.compile(r'build\.(.*)\.(TECHNIQUE=.*)\..*\..*\.log')
+re_num_func = re.compile(r'Number of Functions: (\d+)')
+re_merge = re.compile(r'@.* \+ @.* <= .* Valid: (\d) BinSizes: (\d+) \+ (\d+) <= (\d+) .* Profitable: (\d)')
 
-# Open Database
-def openDatabase(DBFILE):
-    # Only make changes to new database
-    if not os.path.exists(DBFILE):
-        print(f"Creating new database: {DBFILE}")
-        conn = sqlite3.connect(DBFILE)
-        return conn
-    else:
-        print(f"ERROR: {DBFILE} already exists")
-        exit()
-
-# Initialise the database
-def initialiseDatabase(conn):
-    cursor = conn.cursor()
-
-    # Create Benchmarks table
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Benchmarks (
-        BenchmarkName TEXT NOT NULL
-    )
-    ''')
-
-    # Create Flags table
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Flags (
-        FLAG TEXT NOT NULL
-    )
-    ''')
-
-    # Create MatchInfo table
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS MatchInfo (
-        BenchmarkID INTEGER NOT NULL,
-        FlagID INTEGER NOT NULL,
-        Valid INTEGER NOT NULL,
-        BinSize1 INTEGER NOT NULL,
-        BinSize2 INTEGER NOT NULL,
-        MergedBinSize INTEGER NOT NULL,
-        Profitable INTEGER NOT NULL,
-        Distance REAL NOT NULL,
-        TotalTime INTEGER NOT NULL,
-        RankingTime INTEGER NOT NULL,
-        AlignTime INTEGER NOT NULL,
-        CodegenTime INTEGER NOT NULL,
-        VerifyTime INTEGER NOT NULL,
-        UpdateTime INTEGER NOT NULL,
-        FOREIGN KEY (BenchmarkID) REFERENCES Benchmarks(ROWID),
-        FOREIGN KEY (FlagID) REFERENCES Flags(ROWID)
-    )
-    ''')
-
-# Inserts a benchmark name into Benchmarks table
-# If the name already exists, it will return the ROWID of the existing row
-# If the name doesn't exist, it will insert it and return the ROWID of the new row
-def insertBenchmark(conn, BenchmarkName):
-    print(f"Inserting Benchmark: {BenchmarkName}")
-    cursor = conn.cursor()
-
-    # Check if benchmark already exists
-    cursor.execute("SELECT ROWID FROM Benchmarks WHERE BenchmarkName == ?", (BenchmarkName,))
-    result = cursor.fetchone()
-
-    # Benchmark exists
-    if result:
-        return result[0]
-    # Benchmark doesn't exist
-    else:
-        cursor.execute("INSERT INTO Benchmarks (BenchmarkName) VALUES (?)", (BenchmarkName,))
-        conn.commit()
-
-        # Return the ROWID of the newly inserted row
-        return cursor.lastrowid
-
-# Inserts Flag into Flags table
-# If the name already exists, it will return the ROWID of the existing row
-# If the name doesn't exist, it will insert it and return the ROWID of the new row
-def insertFlag(conn, Flag):
-    print(f"Inserting Flag: {Flag}")
-    cursor = conn.cursor()
-
-    # Check if flag exists
-    cursor.execute("SELECT ROWID FROM Flags WHERE FLAG == ?", (Flag,))
-    result = cursor.fetchone()
-
-    if result:
-        return result[0]
-    else:
-        cursor.execute("INSERT INTO Flags (FLAG) VALUES (?)", (Flag,))
-        conn.commit()
-        return cursor.lastrowid
-
-def insertMatchInfo(conn, BenchmarkID, FlagID, data):
-    cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO MatchInfo (BenchmarkID, FlagID, Valid, BinSize1, BinSize2, MergedBinSize, Profitable, Distance, TotalTime, RankingTime, AlignTime, CodegenTime, VerifyTime, UpdateTime)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ''',
-    (BenchmarkID, FlagID, data['valid'], data['bin_size_1'], data['bin_size_2'], data['bin_size_merged'], data['profitable'], data['distance'], data['total_time'], data['ranking_time'], data['align_time'], data['codegen_time'], data['verify_time'], data['update_time']), )
-    conn.commit()
-
-# Main Function
 if __name__ == "__main__":
+    metrics = []
     args = parse_args()
 
     # Script Arguments
     LOG_DIR = args.log_dir
     OUTPUT = args.output
 
-    # Connect to the database
-    conn = openDatabase(OUTPUT)
-    initialiseDatabase(conn)
-
     # Get list of files in LOG_DIR
     files = os.listdir(LOG_DIR)
+    files.sort()
     print(f"Found {len(files)} files in {LOG_DIR}")
 
-    # Regular Expression Pattern for the log file
-    d_pat = r"(\d*)"  # At least one digit
-    sci_pat = r"([\d\.e\-\+]+)"  # Scientific notation pattern that handles both e+ and e-
-    pattern = fr"Valid:\s+{d_pat}.*?BinSizes:\s+{d_pat}\s+\+\s+{d_pat}\s+<=\s+{d_pat}.*?Profitable:\s+{d_pat}.*?Distance:\s+{sci_pat}.*?TotalTime:\s+{d_pat}.*?RankingTime:\s+{d_pat}.*?AlignTime:\s+{d_pat}.*?CodegenTime:\s+{d_pat}.*?VerifyTime:\s+{d_pat}.*?UpdateTime:\s+{d_pat}"
-    file_pattern = r'build\.(.*)\.(TECHNIQUE=.*)\.\d*\-\d*\-\d*.*'
-
     for file in files:
-        # Only read log files
-        if not file.endswith('.log'):
+        # Extract each file's name
+        print(f'File: {file}')
+        filename_match = re.search(re_filename, file)
+        if not filename_match:
             continue
-        print(f"Processing File: {file}")
+        print(f'Benchmark: {filename_match.group(1)}')
+        print(f'Flags: {filename_match.group(2)}')
 
-        # Get the build conditions
-        file_match = re.search(file_pattern, file)
-        print(f"BENCHMARK: {file_match.group(1)}")
-        print(f"FLAGS: {file_match.group(2)}")
-
-        # Insert the benchmark and flag into the database
-        benchmark_id = insertBenchmark(conn, file_match.group(1))
-        flag_id = insertFlag(conn, file_match.group(2))
+        # Extract log information
+        total_functions = 0
+        merge_attempts = 0
+        profitable_merges = 0
+        bin_size_diff = 0
 
         # Read the file
-        with open(os.path.join(LOG_DIR, file), 'r') as file:
-            count = 0
-            for line in file:
-                match = re.search(pattern, line)
-                if match:
-                    data = {
-                        'valid': int(match.group(1)),
-                        'bin_size_1': int(match.group(2)),
-                        'bin_size_2': int(match.group(3)),
-                        'bin_size_merged': int(match.group(4)),
-                        'profitable': int(match.group(5)),
-                        'distance': float(match.group(6)),
-                        'total_time': int(match.group(7)),
-                        'ranking_time': int(match.group(8)),
-                        'align_time': int(match.group(9)),
-                        'codegen_time': int(match.group(10)),
-                        'verify_time': int(match.group(11)),
-                        'update_time': int(match.group(12))
-                    }
-                    if not __debug__:
-                        print(data)
-                    count += 1
-                    insertMatchInfo(conn, benchmark_id, flag_id, data)
-            print(f"TOTAL: {count}")
+        filepath = os.path.join(LOG_DIR, file)
+        with open(filepath, 'r') as log_file:
+            for line in log_file:
+                # Find the number of functions
+                num_func_match = re.search(re_num_func, line)
+                if num_func_match:
+                    total_functions = int(num_func_match.group(1))
+                    print(f'Total Functions: {total_functions}')
+
+                # Find the merge attempts
+                merge_match = re.search(re_merge, line)
+                if merge_match:
+                    # Update the number of merge attempted
+                    merge_attempts += 1
+
+                    # Check if the merge was profitable
+                    if int(merge_match.group(5)) == 1:
+                        profitable_merges += 1
+
+                    # Get the binary sizes
+                    bin_size_1 = int(merge_match.group(2))
+                    bin_size_2 = int(merge_match.group(3))
+                    merged_bin_size = int(merge_match.group(4))
+
+                    # Calculate the difference in binary sizes
+                    bin_size_diff += merged_bin_size - (bin_size_1 + bin_size_2)
         
-    conn.close()
+        print(f'Merge Attempts: {merge_attempts}')
+        print(f'Profitable Merges: {profitable_merges}')
+        print(f'Bin Size Difference: {bin_size_diff}')
+
+        metrics.append([filename_match.group(1), filename_match.group(2), total_functions, merge_attempts, profitable_merges, bin_size_diff])
+        print('===================================')
+
+    # Save the data to a CSV file
+    columns = ['Benchmark', 'Flags', 'TotalFunctions', 'MergeAttempts', 'ProfitableMerges', 'BinSizeDifference']
+    data = pandas.DataFrame(metrics, columns=columns)
+    data.to_csv(OUTPUT, index=False)
+    print(f'Data saved to {OUTPUT}')
